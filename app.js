99474121cb45b3df84ae350a696c22a0bd305c6a
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-05-02 13:16:01 -0500

    square added on button click with size determined by input

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 // let side = document.
-let max = 600;
-let min = 100;
+let min
+let max = 600 - min;
 let container = document.getElementById('shape-container');
-// define and add click listeners to shape buttons
-let btnRectangle = document.getElementById('btn-rectangle');
-btnRectangle.addEventListener('click', addShape);
-let btnCircle = document.getElementById('btn-circle');
-btnCircle.addEventListener('click', addShape);
-let btnSquare = document.getElementById('btn-square');
-btnSquare.addEventListener('click', addShape);
-let btnTriangle = document.getElementById('btn-triangle');
-btnTriangle.addEventListener('click', addShape);
 
 function addShape() {
     let xVal = randomNum(0, 500);
@@ -23,7 +14,7 @@ function randomNum(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function isNumberKey(evt){
+function isNumberKey(evt) {
     let charCode = (evt.which) ? evt.which : evt.keyCode
     if (charCode > 31 && (charCode < 48 || charCode > 57))
         // document.querySelector('input').value = null;
@@ -34,10 +25,10 @@ function isNumberKey(evt){
 class Shape {
     constructor(x, y) {
         this.div = document.createElement('div');
-        this.div.classList.add('shape');
+        // this.div.classList.add('shape');
         this.div.style.left = `${x}px`
         this.div.style.top = `${y}px`
-        this.div.style.backgroundColor = 'navy';
+        // this.div.style.backgroundColor = 'navy';
         container.appendChild(this.div);
 
         this.div.addEventListener("click", () => this.describe());
@@ -58,27 +49,53 @@ class Shape {
 class Circle extends Shape {
     constructor(x, y, radius) {
         super(x, y);
-        this.div.style.backgroundColor = 'blueviolet';
     }
 }
 
 class Triangle extends Shape {
     constructor(x, y, height) {
         super(x, y);
-        this.div.style.backgroundColor = 'gold';
     }
 }
 
 class Rectangle extends Shape {
     constructor(x, y, height, width) {
         super(x, y);
-        this.div.style.backgroundColor = 'lightgreen';
     }
 }
 
 class Square extends Shape {
     constructor(x, y, side) {
         super(x, y);
-        this.div.style.backgroundColor = 'tomato';
+        this.div.classList.add('square');
+        this.side = parseInt(side);
+        this.div.style.height = `${this.side}px`;
+        this.div.style.width = `${this.side}px`;
+        this.updateColor();
+        // let sqDiv = document.createElement('div');
+        // this.sqDiv.addSquare();
+    }
+
+    updateColor() {
+        let newColor = `rgb(${randomNum(0, 255)}, ${randomNum(0, 255)}, ${randomNum(0, 255)})`;
+        this.div.style.background = newColor;
     }
 }
+
+function addSquare() {
+    side = document.getElementById('sqSide').value;
+    console.log(side);
+    xVal = randomNum(0, 500);
+    yVal = randomNum(0, 500);
+    console.log(xVal, yVal);
+    let square = new Square(xVal, yVal, side);
+}
+// define and add click listeners to shape buttons
+let btnRectangle = document.getElementById('btn-rectangle');
+btnRectangle.addEventListener('click', addShape);
+let btnCircle = document.getElementById('btn-circle');
+btnCircle.addEventListener('click', addShape);
+let btnSquare = document.getElementById('btn-square');
+btnSquare.addEventListener('click', addSquare);
+let btnTriangle = document.getElementById('btn-triangle');
+btnTriangle.addEventListener('click', addShape);
